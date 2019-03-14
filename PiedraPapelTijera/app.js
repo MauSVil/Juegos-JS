43dbd12c2337ffdbe23df798bc8f79e47565a4ca
MauSVil <maujr10@hotmail.com>
2019-03-13 22:56:51 -0600

    Funciona

--- PiedraPapelTijera/app.js
+++ PiedraPapelTijera/app.js
@@ -1,7 +1,7 @@
 
 var contador1 = 0;
 var contador2 = 0;
-var global_option;
+// var global_option;
 var option;
 
 
@@ -16,36 +16,49 @@ papel.addEventListener("click", escogistePapel);
 piedra.addEventListener("click", escogistePiedra);
 tijera.addEventListener("click", escogisteTijera);
 
+
+
 function escogistePapel(){
   var option= 3;
-  global_option = option;
+  num = random(1,3)
+  // global_option = option;
+  funciona(option);
+  comprobacion(num, option);
 }
 
 function escogistePiedra(){
   var option = 1;
-  global_option = option;
+  // global_option = option;
+  num = random(1,3)
+  funciona(1);
+  comprobacion(num, 1);
 }
 
 function escogisteTijera(){
   var option = 2;
-  global_option = option;
+  // global_option = option;
+  funciona(2);
+  comprobacion(random(1,3),2);
 }
 
-switch (global_option) {
-  case 1:
+
+function funciona(global_option){
+  switch (global_option) {
+    case 1:
     console.log("Escogiste Piedra");
     break;
 
-  case 2:
+    case 2:
     console.log("Escogiste Tijera");
     break;
 
-  case 3:
+    case 3:
     console.log("Escogiste Papel");
     break;
 
-  default:
+    default:
     console.log("El global_option es: "+ global_option);
+  }
 }
 
 
@@ -57,43 +70,43 @@ switch (global_option) {
     // while ((num !==1) || (num !== 2) || (num !== 3)){
       //   num = prompt('Escribe un numero: 1) Piedra, 2) Papel, 3) Tijeras')
       // }
-      var minimo = 1;
-      var maximo = 3;
 
-      var numero = Math.floor(Math.random()*(maximo-minimo +1)+ minimo);
+function random(minimo, maximo){
+  return Math.floor(Math.random()*(maximo-minimo +1)+ minimo);
+}
 
-      if (numero == global_option) {
-        alert('Es un empate')
-      }
 
-      else if (numero == 1 && global_option == 2){
-        alert('Ganaste');
-        contador1++;
-      }
-      else if (numero == 1 && global_option == 3){
-        alert('Perdiste');
-        contador2++;
-      }
-      else if (numero == 2 && global_option == 3){
-        alert('Ganaste');
-        contador1++;
-      }
-      else if (numero == 2 && global_option == 1){
-        alert('Perdiste');
-        contador2++;
-      }
-      else if(numero == 3 && global_option == 2){
-        alert('Perdiste');
-        contador2++;
-      }
-      else if (numero == 3 && global_option == 1){
-        alert('Ganaste');
-        contador1++;
-      }
-  // }
 
-// console.log('Computadora: ' + contador2);
-// console.log('Usuario: ' + contador1);
+function comprobacion(numero, global_option){
+  if (numero == global_option) {
+    alert('Es un empate')
+  }
+
+  else if (numero == 1 && global_option == 2){
+    alert('Ganaste');
+    contador1++;
+  }
+  else if (numero == 1 && global_option == 3){
+    alert('Perdiste');
+    contador2++;
+  }
+  else if (numero == 2 && global_option == 3){
+    alert('Ganaste');
+    contador1++;
+  }
+  else if (numero == 2 && global_option == 1){
+    alert('Perdiste');
+    contador2++;
+  }
+  else if(numero == 3 && global_option == 2){
+    alert('Perdiste');
+    contador2++;
+  }
+  else if (numero == 3 && global_option == 1){
+    alert('Ganaste');
+    contador1++;
+  }
+}
 
 if(contador2 == 2){
   alert('Perdiste 2 de 3');
